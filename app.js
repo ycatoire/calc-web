eca7abb58896986a009cb4a0c93c9a643b2eed40
Youri Catoire <youri.catoire@gmail.com>
2018-10-27 21:50:59 +0300

    Calc-web app initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function getSum() {
+  var a = $("#a").val()
+  var b = $("#b").val()
+
+  console.log("Summing: " + a + " and " + b);
+
+  $.get(`https://${document.domain}/api/sum/?a=` + a + "&b=" + b, function( data ) {
+    $("body")
+      .append(" Status: " + data.Status)
+	  .append(" Result: " + data.Result + "<br>")
+    }, "json");
+
+}
+
+function getDiff() {
+  var a = $("#aa").val()
+  var b = $("#bb").val()
+
+  console.log("Difference between: " + a + " and " + b);
+
+  $.get(`https://${document.domain}/api/sum/?a=` + a + "&b=" + b, function( data ) {
+    $("body")
+      .append(" Status: " + data.Status)
+      .append(" Result: " + data.Result + "<br>")
+    }, "json");
+
+}
